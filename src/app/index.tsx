f49d0009c950b5d7cf446c6f751befcc7ab14eee
Lee Nelson <lee.nelson.-nd@disneystreaming.com>
2024-07-08 15:14:22 -0700

    removes prettier config

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './components/IFrame';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import IFrame from "./components/IFrame";
 
-const domNode = document.getElementById('react-page');
+const domNode = document.getElementById("react-page");
 const root = ReactDOM.createRoot(domNode);
-root.render(<App />);
+root.render(<IFrame />);
